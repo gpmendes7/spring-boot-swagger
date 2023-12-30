0eba51e221ea5a1266ea7c0208c2c030ec5a7106
gpmendes7 <andrewsseven@gmail.com>
2023-12-30 10:00:51 -0300

    first commit

---
+++ src/test/java/com/hendisantika/springboot/swagger/SpringbootSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboot.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringbootSwaggerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
